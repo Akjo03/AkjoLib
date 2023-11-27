a5466d73fb451b4b7901559a49316361b0b1660d
Lukas Küffer <lukas.kueffer@outlook.com>
2023-11-27 09:25:53 +0100

    Fixed annotation processing for potentially dangerous methods. Also bumped version to 3.8.1.

--- src/main/java/io/github/akjo03/lib/annotation/PotentiallyDangerousProcessor.java
+++ src/main/java/io/github/akjo03/lib/annotation/PotentiallyDangerousProcessor.java
@@ -1,9 +1,8 @@
 package io.github.akjo03.lib.annotation;
 
-import javax.annotation.processing.AbstractProcessor;
-import javax.annotation.processing.RoundEnvironment;
-import javax.annotation.processing.SupportedAnnotationTypes;
-import javax.annotation.processing.SupportedSourceVersion;
+import com.google.auto.service.AutoService;
+
+import javax.annotation.processing.*;
 import javax.lang.model.SourceVersion;
 import javax.lang.model.element.Element;
 import javax.lang.model.element.TypeElement;
@@ -12,6 +11,7 @@ import java.util.Set;
 
 @SupportedAnnotationTypes("io.github.akjo03.lib.annotation.PotentiallyDangerous")
 @SupportedSourceVersion(SourceVersion.RELEASE_8)
+@AutoService(Processor.class)
 @SuppressWarnings("unused")
 public class PotentiallyDangerousProcessor extends AbstractProcessor {
     @Override
